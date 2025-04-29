62ca251e89268c42a35f658f8298284fc566e780
Jimena Sánchez <mjsp674@hotmail.com>
2025-04-29 12:34:06 -0500

    Se instalan módulos, crea la estructura de carpetas, se modifica package.json, se crea .env, se crea .gitignore

---
+++ config/db.js
@@ -0,0 +1,3 @@
+// Importar módulos
+const mysql = require("mysql2"); // Acceder a la librería de mysql2 para conectarse con MySQL
+const dotenv = require("dotenv"); // Acceder a las variables del archivo env
